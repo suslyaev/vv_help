f26c45d6f0e02e98128359fbe6ce591c53379c7d
suslyaev <alex-axe@mail.ru>
2025-10-03 22:27:17 +0300

    Теги в аналитику

--- tickets/templatetags/ticket_filters.py
+++ tickets/templatetags/ticket_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from datetime import timedelta
 
 register = template.Library()
 
@@ -23,3 +24,35 @@ def strip(value):
     if value is None:
         return ''
     return str(value).strip()
+
+@register.filter
+def format_timedelta(value):
+    """Форматирует timedelta в читаемый вид"""
+    if not value or not isinstance(value, timedelta):
+        return ''
+    
+    total_seconds = int(value.total_seconds())
+    
+    if total_seconds < 60:
+        return f"{total_seconds} сек"
+    elif total_seconds < 3600:
+        minutes = total_seconds // 60
+        seconds = total_seconds % 60
+        if seconds > 0:
+            return f"{minutes} мин {seconds} сек"
+        else:
+            return f"{minutes} мин"
+    elif total_seconds < 86400:
+        hours = total_seconds // 3600
+        minutes = (total_seconds % 3600) // 60
+        if minutes > 0:
+            return f"{hours} ч {minutes} мин"
+        else:
+            return f"{hours} ч"
+    else:
+        days = total_seconds // 86400
+        hours = (total_seconds % 86400) // 3600
+        if hours > 0:
+            return f"{days} дн {hours} ч"
+        else:
+            return f"{days} дн"
